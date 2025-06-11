33c52d4f7dd14b645f916d45ecd25e2583bd1946
Sakshi <128570339+sakshi7522@users.noreply.github.com>
2025-06-11 15:47:15 +0530

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -2,24 +2,18 @@ import random
 import string
 
 
-password_length = 8
+password_length = int(input("Enter the required Password length : "))
 
 AllValues= string.ascii_letters + string.digits + string.punctuation
 
-
-
 # USING NORMAL LOOP
-
 # Password = ""
 # for i in range(password_length):
 #     Password += random.choice(AllValues)
 
 
-
 # USING LIST COMPREHENSION
-
 Password = "".join([random.choice(AllValues) for i in range(password_length)])
-
 
 print("Your random Password is : ",Password)
 
